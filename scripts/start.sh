e257b2ea02fc0d4a1191ec70578958ab63bd4db9
tingyuchang <tingyugenius@gmail.com>
2021-08-29 20:11:40 +0800

    fixed docker compose network

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "use ccurrent folder..."
-BBSHOME=$(pwd)
 
-#sed -i '' 's/{{BBSHOME}}/'$BBSHOME'/g' docker_compose.env 
 sed -i '' "s|{{BBSHOME}}|$(pwd)|" docker_compose.env 
+./scripts/docker_initbbs.sh $(pwd) pttofficialapps/go-pttbbs:latest
